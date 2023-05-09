ecf0f2c0321f68cc2ad8b20e476911b2660e5c4b
Olga Barnikova <barnikovaolga8@gmail.com>
2023-05-09 11:49:39 +0300

    in React app i used axios library for API

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import './App.css';
+import ImagesList from "./components/ImagesList";
+
+
+
+function App() {
+  
+  return (
+    <div>
+      <ImagesList />
+    </div> 
+  )    
+}
+
+export default App;
---
+++ src/components/ImagesList.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function getPhotos(page = 1, limit = 10) {
+    return axios
+      .get(`https://picsum.photos/v2/list?page=${page}&limit=${limit}`)
+      .then((response) => response.data);
+  }
+
+const ImagesList = () => {
+    const [page, setPage] = useState(1);
+    const [images, setImages] = useState([]);
+    const [isLoading, setLoading] = useState(true);    
+    
+    useEffect(() => {  
+        getPhotos(page)                    
+          .then(data => { 
+            setImages((prevValue) => [...prevValue, ...data]);           
+            setLoading(false);  
+          })      
+    }, [page]);
+    
+
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
+
+    const handleClick = () => {
+        setLoading(true);
+        setPage((prevValue) => ++prevValue);
+    };
+
+    return (
+        <>
+          <div className="container__photos">
+            {images.map(({ id, download_url: url, author: alt }) => (          
+            <img key= {id} src={url} alt={alt} className="photos" />  
+            ))}            
+        </div>
+        <button onClick={handleClick} className="btn">Show more</button>       
+        </>    
+    )
+};
+
+export default ImagesList;
